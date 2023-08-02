2f5ec557f64fe8fda2295c667d2016ac699e2bb0
Steven Schulteis <schult@gmail.com>
2023-08-02 17:36:12 -0500

    Init hardware and audio callback

--- main.cpp
+++ main.cpp
@@ -1,3 +1,23 @@
-int main() {
-    return 0;
+#include <daisy_seed.h>
+
+//=============================================================================
+void processAudioBlock(
+    daisy::AudioHandle::InputBuffer in,
+    daisy::AudioHandle::OutputBuffer out,
+    size_t size)
+{
+    for (size_t i = 0; i < size; ++i)
+    {
+        out[0][i] = in[0][i];
+        out[1][i] = in[1][i];
+    }
+}
+
+//=============================================================================
+int main()
+{
+    daisy::DaisySeed hardware;
+    hardware.Init();
+    hardware.StartAudio(processAudioBlock);
+    while(true) {}
 }
