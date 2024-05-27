383581e63fafc7c878801f32e11033b7eabff777
Steven Schulteis <schult@gmail.com>
2024-05-27 13:37:53 -0500

    Clean up mapping api

--- util/Mapping.h
+++ util/Mapping.h
@@ -10,25 +10,21 @@ class LinearMapping
 {
 public:
     // min < max
-    constexpr LinearMapping(float _min, float _max) :
-        min(_min),
-        max(_max)
+    constexpr LinearMapping(float min, float max) :
+        _min(min),
+        _max(max)
     {
     }
 
     // 0 <= x <= 1
     float operator()(float x) const
     {
-        return std::lerp(min, max, x);
+        return std::lerp(_min, _max, x);
     }
 
-    constexpr float clamp(float y) const
-    {
-        return std::clamp(y, min, max);
-    }
-
-    const float min;
-    const float max;
+private:
+    const float _min;
+    const float _max;
 };
 
 class LogMapping
@@ -37,22 +33,18 @@ public:
     // min < center < max
     // center < (min + max) / 2
     // min + center > 0
-    constexpr LogMapping(float _min, float _center, float _max) :
-        min(_min),
-        max(_max),
-        offset(calculateOffset(_min, _center, _max)),
-        log_min(gcem::log(_min + offset)),
-        log_range(gcem::log(_max + offset) - log_min)
+    constexpr LogMapping(float min, float center, float max) :
+        offset(calculateOffset(min, center, max)),
+        log_min(gcem::log(min + offset)),
+        log_range(gcem::log(max + offset) - log_min)
     {
     }
 
     // 0 < min < max
-    constexpr LogMapping(float _min, float _max) :
-        min(_min),
-        max(_max),
+    constexpr LogMapping(float min, float max) :
         offset(0),
-        log_min(gcem::log(_min)),
-        log_range(gcem::log(_max) - log_min)
+        log_min(gcem::log(min)),
+        log_range(gcem::log(max) - log_min)
     {
     }
 
@@ -61,14 +53,6 @@ public:
     {
         return fasterexp(log_range*x + log_min) - offset;
     }
-
-    constexpr float clamp(float y) const
-    {
-        return std::clamp(y, min, max);
-    }
-
-    const float min;
-    const float max;
 
 private:
     static constexpr float calculateCenter(float min, float max)
